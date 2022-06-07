158f756769767d1e7f6f6e2f4bcd9535227dcce4
StefanoMRC <stefano.mrc92@gmail.com>
2022-06-07 15:40:39 +0200

    controllo delle rotte

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guest.home');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+
+
+Route::middleware('auth')
+->prefix('admin')
+->name('admin.')
+->namespace('Admin')
+->group(function () {
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::resource('posts', 'PostController');
+});
