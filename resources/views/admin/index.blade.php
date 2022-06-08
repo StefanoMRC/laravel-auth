6cc185f98da50bfae8d0c882e9852a29c059acd4
StefanoMRC <stefano.mrc92@gmail.com>
2022-06-08 12:49:14 +0200

    aggiunta create e index

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+@section('content')
+<table class="table table-dark table-hover">
+    <thead>
+      <tr>
+        <th scope="col">Titolo</th>
+        <th scope="col">Contenuto</th>
+        <th scope="col">Immagine</th>
+        <th scope="col">Slug</th>
+        <th scope="col">Eventi</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </tbody>
+  </table>
+@endsection
